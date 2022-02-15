3b9ec67972e33ee098451a45036b0fa49d79b8f1
monk3yd <jegajardog@gmail.com>
2022-02-15 06:46:38 -0300

    Email Sender Functionality

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
+import os
+import smtplib
+
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+from wtforms.widgets import TextArea
+
+SECRET_KEY = os.urandom(32)
+
+# TODO Env Variables
+WEB_EMAIL = "monk3yd.thelab@gmail.com"  # WebApp Email
+WEB_PWD = "raftel12345"
+MY_EMAIL = "monk3yd.thelab@yahoo.com"  # My Personal Email
+
+class ContactForm(FlaskForm):
+    name = StringField(label="Name", validators=[DataRequired()], render_kw={"placeholder": "Your Name"})
+    email = EmailField(label="Email", validators=[DataRequired()], render_kw={"placeholder": "Your Email"})
+    subject = StringField(label="Subject", validators=[DataRequired()], render_kw={"placeholder": "Subject"})
+    message = StringField(label="Message", widget=TextArea(), validators=[DataRequired()], render_kw={"placeholder": "Message"})
+    submit = SubmitField(label="Send Message")
+
 
 app = Flask(__name__)
+app.secret_key = SECRET_KEY
 
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=WEB_EMAIL, password=WEB_PWD)
+            connection.sendmail(
+                from_addr=WEB_EMAIL,
+                to_addrs=MY_EMAIL,
+                msg=f"Subject: {form.subject.data}\n\n{form.message.data}"
+            )
+    return render_template("index.html", form=form)
 
 
-@app.route("/portfolio-details")
-def portfolio():
-    return render_template("portfolio-details.html")
+# @app.route("/portfolio-details")
+# def portfolio():
+#     return render_template("portfolio-details.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -203,31 +203,31 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
-
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
+  // new Swiper('.testimonials-slider', {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  //   slidesPerView: 'auto',
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //     clickable: true
+  //   },
+  //   breakpoints: {
+  //     320: {
+  //       slidesPerView: 1,
+  //       spaceBetween: 20
+  //     },
+  //
+  //     1200: {
+  //       slidesPerView: 3,
+  //       spaceBetween: 20
+  //     }
+  //   }
+  // });
 
   /**
    * Porfolio isotope and filter
@@ -276,18 +276,18 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
+  // new Swiper('.portfolio-details-slider', {
+  //   speed: 400,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false
+  //   },
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //     clickable: true
+  //   }
+  // });
 
 })()
